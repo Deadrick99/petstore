2e7f9c77f69b0d5d3af81729f0570e93a5021e82
William <33364879+Deadrick99@users.noreply.github.com>
2022-10-01 01:25:28 -0400

    Added Basic Form for pet adoption

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import Adopt from "./pages/Adopt";
 
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
@@ -12,9 +13,10 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route  path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="*" element={<Page404 />} />
+          <Route  path="/Adopt" element={<Adopt/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
