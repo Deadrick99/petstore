f406c057ad2b79577b8430026def38f6dddde366
johnserra19 <98505595+johnserra19@users.noreply.github.com>
2022-10-01 12:49:58 -0400

    modified:   index.css 	new file:   src/index.d.ts 	modified:   src/pages/Home.tsx 	modified:   src/pages/Navbar.tsx 	new file:   src/pages/images/pets2.png

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
+import image from "./images/pets2.png";
+
+
 const Home = () => {
-  return <h1>Home Page</h1>;
-};
+  return (
+    <div style={{ backgroundImage:`url(${image})`, 
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh'}}>
+    </div>
+    //<h1>Home Page</h1>
+);
+}
 
 export default Home;
