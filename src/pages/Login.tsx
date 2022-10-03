99e3f4b9a0df8ae8cb3b36f4288ced3787f43e54
Shay Snyder <snyderse@outlook.com>
2022-10-03 11:40:56 -0400

    adding email and password fields to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -7,6 +8,36 @@ import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = () => {
+    // Styling Parameters
+    const FormGroupClass = 'mb-3';
+
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    });
+    const [errors, setErrors] = useState({
+        email: null,
+        password: null,
+    })
+
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        console.log(form)
+    }
+
+    const setField = (field: any, value: any) => {
+        setForm({
+            ...form,
+            [field]:value
+        });
+
+        if (errors[field as keyof typeof errors])
+            setErrors({
+                ...errors,
+                [field]:null
+            });
+    }
+
     return (
         <Container>
             <Row className='justify-content-center'>
@@ -15,23 +46,38 @@ const Login = () => {
                         {/**
                          * Email Address Field
                          */}
-                        <Form.Group className='mb-3' controlId='formLoginEmail'>
+                        <Form.Group className={FormGroupClass} controlId='formSigUpEmail'>
                             <Form.Label>Email Address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter your email address...' />
+                            <Form.Control
+                                type='email'
+                                placeholder='Enter your email address...'
+                                value={form.email}
+                                onChange={(e) => setField('email', e.target.value)}
+                                isInvalid={!!errors.email}
+                            />
+                            <Form.Control.Feedback type='invalid'>
+                                {errors.email}
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         {/**
                          * Password Field
                         */}
-                        <Form.Group className='mb-3' controlId='formLoginPassword'>
+                        <Form.Group className={FormGroupClass} controlId='formSignUpPassword'>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder='Enter your password...' />
+                            <Form.Control
+                                type="password"
+                                placeholder='Enter your password...'
+                                value={form.password}
+                                onChange={(e) => setField('password', e.target.value)}
+                                isInvalid={!!errors.password}
+                            />
                         </Form.Group>
 
                         {/**
                          * Buttons
                          */}
-                        <Button variant='primary' type="submit">
+                        <Button variant='primary' type="submit" onClick={handleSubmit}>
                             Submit
                         </Button>
                     </Form>
