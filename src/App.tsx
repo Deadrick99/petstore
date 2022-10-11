df3681ec09361d6a93bf9264414108116b0dc49e
johnserra19 <98505595+johnserra19@users.noreply.github.com>
2022-10-10 22:41:44 -0400

    modified:   src/App.tsx 	modified:   index.css 	modified:   src/pages/Layout.tsx 	deleted:    src/pages/Navbar.tsx 	src/pages/Navbar2.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
@@ -10,7 +11,7 @@ import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import Page404 from "./pages/Page404";
 import SignUp from "./pages/SignUp";
-import Navbar from './pages/Navbar';
+import Navbar from './pages/Navbar2';
 import Pets from "./pages/Pets";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -21,8 +22,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
           <Route index element={<Home />} />
           <Route path="*" element={<Page404 />} />
           <Route  path="/Adopt" element={<Adopt/>}/>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<SignUp />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<SignUp />} />
           <Route path="/Pets" element={<Pets/>}/>
         </Route>
       </Routes>
