bb6b3fba960649d5ac9758e985c4d7a03327f3f4
johnserra19 <98505595+johnserra19@users.noreply.github.com>
2022-10-10 23:57:18 -0400

    modified:   index.css 	modified:   src/pages/Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import image from "./images/pets2.png";
 
-
 const Home = () => {
   return (
-    <div style={{ backgroundImage:`url(${image})`, 
+    <div className="Home">
+
+    </div>
+    /*<div style={{ backgroundImage:`url(${image})`, 
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     width: '100vw',
     height: '100vh'}}>
-    </div>
-    //<h1>Home Page</h1>
+    </div>*/
 );
 }
 
