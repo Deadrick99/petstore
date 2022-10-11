3ad26a44af1911360c4f92851364d00587501277
johnserra19 <98505595+johnserra19@users.noreply.github.com>
2022-10-10 23:04:38 -0400

    modified:   index.css 	modified:   src/pages/Layout.tsx 	deleted:    src/pages/Navbar.tsx 	new file:   src/pages/Navbar2.tsx

---
+++ src/pages/Navbar2.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Navbar2() {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="/">PetStore.com</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown title="Pets" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/Pets">Dogs</NavDropdown.Item>
+              <NavDropdown.Item href="/Pets">Cats</NavDropdown.Item>
+              <NavDropdown.Item href="/Pets">Other</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/Merchandise">Merchandise</Nav.Link>
+            <Nav.Link href="/Suppliers">Suppliers</Nav.Link>
+            <Nav.Link href="/Customers">Customers</Nav.Link>
+            <Nav.Link href="/Adopt">Adopt</Nav.Link>
+          </Nav>
+
+          <Nav className="ml-auto">
+            <Nav.Link href="/Login">Login</Nav.Link>
+            <Nav.Link href="/Signup">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+export default Navbar2;
