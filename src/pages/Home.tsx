f7bcca00e66ef7a5f31af091209199ae092aad72
William <33364879+Deadrick99@users.noreply.github.com>
2022-10-15 20:35:49 -0400

    added hooks to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import image from "./images/pets2.png";
-
+import {useEffect, useState} from "react";
 const Home = () => {
+  const [visited, useVistited] = useState(false);
+  useEffect(()=>{
+  console.log("increment vistor");
+},[visited])
   return (
     <body>
       <div className="Home">  
