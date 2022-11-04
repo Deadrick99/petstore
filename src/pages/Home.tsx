a586599620ab436e19ebad14127ce829d1c0bf49
johnserra19 <98505595+johnserra19@users.noreply.github.com>
2022-11-03 21:47:46 -0400

    modified:   index.css 	modified:   index.html 	new file:   src/pages/Footer.tsx 	modified:   src/pages/Home.tsx 	modified:   src/pages/Layout.tsx 	modified:   src/pages/Navbar2.tsx 	new file:   src/pages/images/petparadiselogo1.png

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import image from "./images/pets2.png";
+import image2 from "./images/petstorelogo1.png";
 import {useEffect, useState} from "react";
 import Col from "react-bootstrap/esm/Col";
 import Card from "react-bootstrap/esm/Card";
@@ -9,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setViews } from "../redux/views";
 import {setStart} from "../redux/time";
 import { setPageStart } from "../redux/pageTime"; 
+import Carousel from 'react-bootstrap/Carousel';
 
 const Home = () => {
   /**
@@ -37,22 +39,44 @@ const Home = () => {
   return (
        
       <div className="Home" >  
-      <div className="row align-items-center vh-100">
-      <Col sm={3} lg={2} className = "m-3 mx-auto p-3">
-            <Card style={{ textAlign: 'center', width:'100%', height:'100%', boxShadow:'10px 5px 5px black' }}>
-            <Row>
-                 <Col>  
-                 <Card.Title>Welcome to the Pet Store</Card.Title>
-                </Col>
-            </Row>
-            </Card>
-      </Col>
+      <Carousel>
+      <Carousel.Item>
+            <img
+              className="d-block mx-auto"
+              style={{ height: 800 }}
+              src="https://imageio.forbes.com/specials-images/dam/imageserve/1068867780/960x0.jpg?format=jpg&width=960"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h1>Thank you for visiting our website!</h1>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+              className="d-block mx-auto"
+              style={{ height: 800 }}
+              src="https://vetsource.com/wp-content/uploads/2018/11/img-pet-adoption-101.jpg"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h1>Find your next best friend</h1>
+              <h2>Adoption applications are open now!</h2>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+              className="d-block mx-auto"
+              style={{ height: 800 }}
+              src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2020%2F07%2F31%2Fvarious-pet-toys-00c3ec73.jpg"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h1>Need some merch?</h1>
+              <h2>Check out our selection of collars, kennels, and more!</h2>
+            </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
       </div>
-      </div>
-    
-     
-    
-    
 );
 }
 
